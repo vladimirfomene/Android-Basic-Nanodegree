a1a6289f411714af47359026c6a720b7226a3e6d
vladimir fomene <vladimirfomene@gmail.com>
2016-11-01 06:26:55 +0000

    add event handling methods for CourtCounter

--- CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayForTeamA(8);
+    }
+
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+
+    /**
+     * Adds two points to our score board
+     */
+    public void plusTwo(View view){
+        int initialScore = 2;
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(initialScore));
+    }
+
+    /**
+     * Adds three points to our score board
+     */
+    public void plusThree(View view){
+        int initialScore = 3;
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(initialScore));
+    }
+
+    /**
+     * Adds one point to the score board
+     */
+    public void freeThrow(View view){
+        int initialScore = 1;
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(initialScore));
     }
 }
