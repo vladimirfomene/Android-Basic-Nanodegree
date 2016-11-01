3eccce929fd8465b21f9c61235c0c3954f99e978
vladimir fomene <vladimirfomene@gmail.com>
2016-11-01 07:50:33 +0000

    Adding views for a new team and adding java code to update new team's scores

--- CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -7,11 +7,16 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    /* Variable keeps track of team A's score */
+    private int scoreTeamA = 0;
+
+    /* Variable keeps track of team B's score */
+    private int scoreTeamB = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayForTeamA(8);
     }
 
 
@@ -23,31 +28,58 @@ public class MainActivity extends AppCompatActivity {
         scoreView.setText(String.valueOf(score));
     }
 
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
 
     /**
-     * Adds two points to our score board
+     * Adds two points to our score board for team A
      */
-    public void plusTwo(View view){
-        int initialScore = 2;
-        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
-        scoreView.setText(String.valueOf(initialScore));
+    public void plusTwoForTeamA(View view){
+        scoreTeamA += 2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Adds two points to our score board for team A
+     */
+    public void plusTwoForTeamB(View view){
+        scoreTeamB += 2;
+        displayForTeamB(scoreTeamB);
     }
 
     /**
      * Adds three points to our score board
      */
-    public void plusThree(View view){
-        int initialScore = 3;
-        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
-        scoreView.setText(String.valueOf(initialScore));
+    public void plusThreeForTeamA(View view){
+        scoreTeamA += 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Adds three points to our score board
+     */
+    public void plusThreeForTeamB(View view){
+        scoreTeamB += 3;
+        displayForTeamB(scoreTeamB);
     }
 
     /**
      * Adds one point to the score board
      */
-    public void freeThrow(View view){
-        int initialScore = 1;
-        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
-        scoreView.setText(String.valueOf(initialScore));
+    public void freeThrowForTeamA(View view){
+        displayForTeamA(++scoreTeamA);
+    }
+
+    /**
+     * Adds one point to the score board
+     */
+    public void freeThrowForTeamB(View view){
+        displayForTeamB(++scoreTeamB);
     }
 }
