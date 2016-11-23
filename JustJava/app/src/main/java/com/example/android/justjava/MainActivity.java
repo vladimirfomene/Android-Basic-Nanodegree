fa5f99a9c96b154374d6eaa4ba2e8491b10de6e6
vladimir fomene <vladimirfomene@gmail.com>
2016-11-23 07:25:01 +0000

    create intent for email and add alternate resources for translation

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -6,14 +6,26 @@
  */
 package com.example.android.justjava;
 
+
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.justjava.R;
 
+import java.net.URI;
 import java.text.NumberFormat;
+
+import static android.R.attr.duration;
 
 /**
  * This app displays an order form to order coffee.
@@ -33,22 +45,47 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price = calculatePrice();
-        String priceMessage =  createOrderSummary(price);
-        displayMessage(priceMessage);
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whippedCream_checkbox);
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
 
+        CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        boolean hasChocolate = chocolateCheckBox.isChecked();
+
+        EditText nameTextField = (EditText) findViewById(R.id.name_view);
+        String name = String.valueOf(nameTextField.getText());
+        int price = calculatePrice(hasWhippedCream, hasChocolate);
+        String priceMessage =  createOrderSummary(price, hasWhippedCream, hasChocolate, name);
+        String[] addresses = {"vladimir.fomene@ashesi.edu.gh"};
+        composeEmail(addresses,"Coffee Order Summary for " + name, priceMessage);
+
+    }
+
+
+    public void composeEmail(String[] addresses, String subject, String message) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
     /**
      * Computes the order summary for our custormers
+     * @param hasWhippedCream tells you whether or not order has whipped cream
+     * @param hasChocolate tells you whether or not order has chocolate
      * @param price per cup of coffee
      * @return summary order summary for a client
      */
-    private String createOrderSummary(int price){
-        String summary = "Name: " + "Vladimir Fomene\n" +
-                         "Quantity" + quantity + "\n" +
-                         "Total $: " + price + "\n" +
-                         "Thank You!";
+    private String createOrderSummary(int price, boolean hasWhippedCream, boolean hasChocolate, String clientName){
+        String summary = getString(R.string.order_summary_name, clientName) + "\n" +
+                         "Add whipped Cream? " + hasWhippedCream + "\n" +
+                         "Add Chocolate? " + hasChocolate + "\n" +
+                         "Quantity: " + quantity + "\n" +
+                         "Total: " + "$" + price + "\n" +
+                         getString(R.string.thank_you);
         return summary;
 
     }
@@ -56,15 +93,31 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Calculates the price of the order.
      */
-    private int calculatePrice() {
-        return quantity * 5;
+    private int calculatePrice(boolean hasWhippedCream, boolean hasChocolate) {
+
+        int basePrice = 5;
+
+        //add price for whipped cream
+        if(hasWhippedCream){ basePrice += 1; }
+
+        //add price for chocolate
+        if(hasChocolate){ basePrice += 2;  }
+
+        return quantity * basePrice;
     }
 
     /*
     *  This method increment the counter for cups of coffee
      */
     public void increment(View view){
-        quantity = quantity + 1;
+
+        //Make sure that you don't exceed 100 cups
+        if(quantity < 100) {
+            quantity = quantity + 1;
+        }else {
+            Toast.makeText(getApplicationContext(), "You can't go above 100 cup", Toast.LENGTH_SHORT).show();
+            return;
+        }
         displayQuantity(quantity);
     }
 
@@ -72,7 +125,14 @@ public class MainActivity extends AppCompatActivity {
     * This method decrement the counter for cups of coffees
      */
     public void decrement(View view){
-        quantity = quantity - 1;
+
+        //Make sure that you don't go below 1 cup
+        if(quantity > 1) {
+            quantity = quantity - 1;
+        }else {
+            Toast.makeText(getApplicationContext(), "You can't go below 1 cup", Toast.LENGTH_SHORT).show();
+            return;
+        }
         displayQuantity(quantity);
     }
 
@@ -85,11 +145,5 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    /**
-     * This method displays the given text on the screen.
-     */
-    private void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
-    }
+
 }
